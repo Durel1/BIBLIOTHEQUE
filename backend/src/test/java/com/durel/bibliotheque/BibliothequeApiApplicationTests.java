5d27e7075c686e1d5ee5ecb2086db17995a4518a
Durel Kenfack <durel.kenfack@et.esiea.fr>
2026-09-01 00:21:51 +0200

    chore: bootstrap Spring Boot backend

---
+++ backend/src/test/java/com/durel/bibliotheque/BibliothequeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durel.bibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
